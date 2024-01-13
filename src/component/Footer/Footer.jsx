e9c4f42018b06f463e840dbbadf6e71fb720874e
Manish Gupta <manishgupta9205@gmail.com>
2024-01-14 00:13:29 +0530

    Adding footer and form component

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
-
+import Form from '../Form/Form'
+import { FaGithub } from "react-icons/fa";
+import { IoLogoLinkedin } from "react-icons/io5";
+import { FaInstagram } from "react-icons/fa";
+import Footerstyle from "./Footer.module.css"
 export default function Footer() {
+  const links_url={
+    "github":"https://github.com/MG-9205",
+    "linkedin":"https://www.linkedin.com/in/manishgupta31",
+    "instagram":"https://www.instagram.com/manish_gupta31/"
+  }
   return (
-    <div>footer</div>
+    <div>
+      <Form/>
+      <div className={Footerstyle.links}>
+      <a href={links_url.github} target='self'><FaGithub className={Footerstyle.icon}/></a> 
+       <a href={links_url.linkedin} target='self'><IoLogoLinkedin className={Footerstyle.icon}/></a> 
+       <a href={links_url.instagram} target='self'><FaInstagram className={Footerstyle.icon}/></a>
+      </div>
+    </div>
   )
 }
---
+++ src/component/Form/Form.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import Formstyle from "./Form.module.css";
+import { FaLocationDot } from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
+import { FaPhoneVolume } from "react-icons/fa6";
+
+export default function Form() {
+  return (
+    <div className={Formstyle.main_div}>
+      <div className={Formstyle.div1}>
+        <div>
+          <h1>Contact us</h1>
+        </div>
+        <div>
+          <form>
+            <div className={Formstyle.contact_detail}>
+              <label htmlFor="name123">
+                <input type="text" id="name123" required="required"/>
+                <span>Name</span>
+              </label>
+            </div>
+            <div className={Formstyle.contact_detail}>
+              <label htmlFor="Email123">
+                <input type="email" id="Email123"  required="required"/>
+                <span>Email</span>
+              </label>
+            </div>
+            <div className={Formstyle.contact_detail}>
+              <label htmlFor="Phone123">
+                <input type="number" id="Phone123"  required="required"/>
+                <span>Phone</span>
+              </label>
+            </div>
+            <div className={Formstyle.contact_detail}>
+              <textarea name="" id="" cols="30" rows="5"  required="required"></textarea>
+              <span>Message</span>
+            </div>
+            <div className={Formstyle.contact_detail}>
+              <button>Send</button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div className={Formstyle.div2}>
+        <div>
+          <h1>Contact information</h1>
+        </div>
+        <div>
+          <i>
+          "Connect with Us for a Stronger You: 
+          Reach Out Today and Let's Sculpt Your Fitness Goals Together!"
+          </i>
+        </div>
+        <div className={Formstyle.div2_box}>
+          <div className={Formstyle.div2_icon}>
+            <FaLocationDot />
+          </div>
+          <div className={Formstyle.div2_text}>
+            <h3>Address</h3>
+            <p>138 west street near signature bridge new delhi 110094</p>
+          </div>
+        </div>
+        <div className={Formstyle.div2_box}>
+          <div className={Formstyle.div2_icon}>
+            <FaPhoneVolume />
+          </div>
+          <div className={Formstyle.div2_text}>
+            <h3>Phone No:</h3>
+            <p>91+ 92050XXX41</p>
+          </div>
+        </div >
+        <div className={Formstyle.div2_box}>
+          <div className={Formstyle.div2_icon}>
+            <MdEmail />
+          </div>
+          <div className={Formstyle.div2_text}>
+            <h3>Email</h3>
+            <p>manishgupta9205@gmail.com</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
