4fb741e9938a307d1654f7c9701502c778c0c8d4
Manish Gupta <manishgupta9205@gmail.com>
2024-01-20 23:29:25 +0530

    project completed

--- src/assets/image/Image_index.jsx
+++ src/assets/image/Image_index.jsx
@@ -17,13 +17,18 @@ import gym from "./gym.jpg"
 import diet from "./nutrition.jpg"
 import swim from "./swim.jpg"
 import intro from "./gym2.jpg"
+import method1 from "./methods.jpg"
+import method2 from "./method2.jpg"
+import method3 from "./method3.jpg"
+import method4 from "./method4.jpg"
 
 const Image_index={
     "slider_image":[pic1,pic2,pic3,pic4,pic5,pic6,pic7,pic8],
     "about_img":[about1,about2,about3],
     "Facilities_img":[pool,locker,training_area],
     "services_img":[cardio,gym,diet,swim],
-    "intro":intro
+    "intro":intro,
+    "methods":[method1,method2,method3,method4]
     
 }
 export default Image_index
--- src/component/component2/Component2.jsx
+++ src/component/component2/Component2.jsx
@@ -1,24 +1,77 @@
 import React from "react";
 import Component2style from "./Component2.module.css";
 import Image_index from "../../assets/image/Image_index";
-import { Link } from "react-router-dom";
 
 export default function Component2() {
+  const methods_detail = [
+    {
+      image: Image_index.methods[0],
+      heading: "Body Shape",
+      Content:
+        "Transform Your Body, Sculpt Your Future: Embrace the Body Shape Method at MH FITNESS !",
+    },
+    {
+      image: Image_index.methods[1],
+      heading: "Extreme",
+      Content:
+        "Unleash Your Potential with the Extreme Method: Redefine Your Limits at MH FITNESS!",
+    },
+    {
+      image: Image_index.methods[2],
+      heading: "Burn",
+      Content:
+        "Ignite Your Fitness Journey: Embrace the Burn Method for a Stronger, Fitter You at MH FITNESS!",
+    },
+    {
+      image: Image_index.methods[3],
+      heading: "Hiit",
+      Content:
+        "Elevate Your Fitness: Unleash the Power of HIIT at MH FITNESS  Where Results Meet Intensity!",
+    },
+  ];
   return (
-    <>
+    <><div className={Component2style.super_div}>
       <div className={Component2style.main_div}>
         <div className={Component2style.contanier_div}>
-            <div className={Component2style.Div1}></div>
+          <div className={Component2style.Div1}></div>
           <div className={Component2style.img_div}>
             <img src={Image_index.intro} alt="" />
           </div>
         </div>
       </div>
-      <div>
-        <div>
-            
+      <div className={Component2style.method_heading}>
+        <h1>OUR METHODS</h1>
+      </div>
+      <div className={Component2style.method_div}>
+        <div className={Component2style.method_bg}></div>
+        <div className={Component2style.methods_container}>
+          {methods_detail.map((item, index) => (
+            <div className={Component2style.method}>
+              <div className={Component2style.method_img} key={index}>
+                <img src={item.image} alt="" />
+              </div>
+              <div className={Component2style.method_content}>
+                <h2>{item.heading}</h2>
+                <p>"{item.Content}"</p>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
+      <div className={Component2style.method_mobile_div}>
+        <div className={Component2style.method_mobile_img}>
+          <img src={Image_index.methods[1]} alt="" />
+        </div>
+        <div className={Component2style.method_mobile_content}>
+        {methods_detail.map((item, index) => (
+              <div className={Component2style.method_mobile}>
+                <h2>{item.heading}</h2>
+                <p>"{item.Content}"</p>
+              </div>
+            
+          ))}
+        </div>
+      </div></div>
     </>
   );
 }
