98c6ad2f2a518f4e9212493f07cbb6ad5df31da8
Manish Gupta <manishgupta9205@gmail.com>
2024-01-22 00:27:03 +0530

    10

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Layout from './Layout'
+import Layout from './Layout.jsx'
 import About from './component/About/About'
 import App from './App.jsx'
 import './index.css'
